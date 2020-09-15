7421583f03777b4e7178446c4ec5db19c345f092
iLeif96 <ileif@yandex.ru>
2020-09-15 16:15:50 +0300

    Just still trying

--- Killer/Program.cs
+++ Killer/Program.cs
@@ -7,6 +7,7 @@ namespace Killer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Maby ill destroy this");
         }
     }
 }
